11c929ea7d6d331ea95ec6887bb0a6fdcdd08f8f
Sam Lockart <sam.lockart@zendesk.com>
2022-12-05 23:28:19 +1100

    Work on getting common pairs

--- day-3/src/main.rs
+++ day-3/src/main.rs
@@ -1,6 +1,8 @@
+use std::collections::{HashMap, HashSet};
 use std::fs::File;
 use std::io::{self, Read};
 
+#[derive(Debug)]
 struct Rucksack<'a>(&'a str, &'a str);
 
 impl<'a> From<(&'a str, &'a str)> for Rucksack<'a> {
@@ -16,7 +18,28 @@ fn main() {
         Err(e) => panic!("{}", e),
     };
 
-    let _rucksacks = parse_input(&input_data);
+    let rucksacks = parse_input(&input_data);
+
+    for r in rucksacks {
+        get_unique(r);
+    }
+}
+
+fn get_unique(r: Rucksack) -> Vec<u8> {
+    println!("{:?}", r);
+    let mut unique_map: HashMap<u8, bool> = HashMap::with_capacity(26 * 2); // alphabet * 2
+    let _ = r.0.as_bytes().iter().map(|b| unique_map.insert(*b, true));
+
+    println!("{:?}", unique_map);
+
+    let unique =
+        r.1.as_bytes()
+            .iter()
+            .copied()
+            .filter(|b| unique_map.contains_key(b))
+            .collect::<Vec<u8>>();
+    println!("{:?}", unique);
+    unique
 }
 
 fn read_input(path: &str) -> Result<String, io::Error> {
@@ -54,20 +77,7 @@ mod tests {
 
         let rucksacks = parse_input(input_data.as_ref().unwrap().as_str());
         for rucksack in rucksacks {
-            println!(
-                "{}[{}] : {}[{}]",
-                rucksack.0,
-                rucksack.0.len(),
-                rucksack.1,
-                rucksack.1.len()
-            );
-            for b in rucksack.0.as_bytes() {
-                if b.is_ascii_lowercase() {
-                    println!("{}", b - 96);
-                } else {
-                    println!("{}", b - 38);
-                }
-            }
+            get_unique(rucksack);
         }
     }
 }
