dfe58990091facc23231af631b713f91967eebc1
Sam Lockart <sam.lockart@zendesk.com>
2022-12-07 23:31:13 +1100

    Finished part 2

--- day-4/src/main.rs
+++ day-4/src/main.rs
@@ -1,21 +1,27 @@
 use std::fs::File;
-use std::io::{self, BufRead, Read};
+use std::io::{self, Read};
 
 fn main() {
-    part_one();
+    result();
 }
 
-fn part_one() {
+fn result() {
     let input = read_input("include/input").unwrap();
 
-    let mut overlapping = 0;
+    let mut full_overlap = 0;
+    let mut partial_overlap = 0;
+
     for line in input.lines() {
         let pairs = get_pairs(line);
         if is_overlapping(&pairs[..]) {
-            overlapping += 1;
+            full_overlap += 1;
+        }
+        if is_partial_overlap(&pairs[..]) {
+            partial_overlap += 1;
         }
     }
-    println!("overlapping pairs: {}", overlapping);
+    println!("part one: {}", full_overlap);
+    println!("part two: {}", partial_overlap);
 }
 
 fn read_input(path: &str) -> Result<String, io::Error> {
@@ -48,9 +54,16 @@ fn get_pairs(line: &str) -> Vec<u8> {
     r
 }
 
+fn is_partial_overlap(pairs: &[u8]) -> bool {
+    match pairs {
+        [x1, x2, y1, y2] => x1.le(y2) && y1.le(x2),
+        _ => panic!("incorrectly sized slice provided! must be a pair of pairs"),
+    }
+}
+
 fn is_overlapping(pairs: &[u8]) -> bool {
     match pairs {
-        [l1, r1, l2, r2] => l1.le(l2) && r1.ge(r2) || l2.le(l1) && r2.ge(r1),
+        [x1, x2, y1, y2] => x1.le(y1) && x2.ge(y2) || y1.le(x1) && y2.ge(x2),
         _ => panic!("incorrectly sized slice provided! must be a pair of pairs"),
     }
 }
@@ -94,6 +107,41 @@ mod tests {
     }
 
     #[test]
+    fn test_is_partial_overlap() {
+        {
+            let pairs: &[u8; 4] = &[2, 3, 4, 5];
+            let got = is_overlapping(pairs);
+            assert!(!got);
+        }
+        {
+            let pairs: &[u8; 4] = &[2, 4, 6, 8];
+            let got = is_overlapping(pairs);
+            assert!(!got);
+        }
+
+        {
+            let pairs: &[u8; 4] = &[5, 7, 7, 9];
+            let got = is_partial_overlap(pairs);
+            assert!(got);
+        }
+        {
+            let pairs: &[u8; 4] = &[2, 8, 3, 7];
+            let got = is_partial_overlap(pairs);
+            assert!(got);
+        }
+        {
+            let pairs: &[u8; 4] = &[6, 6, 4, 6];
+            let got = is_partial_overlap(pairs);
+            assert!(got);
+        }
+        {
+            let pairs: &[u8; 4] = &[2, 6, 4, 8];
+            let got = is_partial_overlap(pairs);
+            assert!(got);
+        }
+    }
+
+    #[test]
     fn test_part_one() {
         let input = read_input("include/example").unwrap();
 
@@ -105,5 +153,22 @@ mod tests {
             }
         }
         assert_eq!(overlapping, 2);
+    }
+
+    #[test]
+    fn test_part_two() {
+        let input = read_input("include/example").unwrap();
+
+        let mut overlapping = 0;
+        for line in input.lines() {
+            let pairs = get_pairs(line);
+            if is_partial_overlap(&pairs[..]) {
+                overlapping += 1;
+                println!("{} (overlapping)", line);
+            } else {
+                println!("{}", line);
+            }
+        }
+        assert_eq!(overlapping, 4);
     }
 }
