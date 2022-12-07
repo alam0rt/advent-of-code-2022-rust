2b28139664d75556cde2f83205febca76252e8a6
Sam Lockart <sam.lockart@zendesk.com>
2022-12-07 15:02:00 +1100

    finish part 2 - very ugly

--- day-3/src/main.rs
+++ day-3/src/main.rs
@@ -1,4 +1,4 @@
-use std::collections::HashSet;
+use std::collections::{HashMap, HashSet};
 use std::fs::File;
 use std::io::{self, Read};
 
@@ -21,7 +21,8 @@ fn main() {
     let rucksacks = parse_input(input_data.as_str());
     let mut sum: i32 = 0;
     for rucksack in rucksacks {
-        let common_items = get_unique(rucksack);
+        let r = vec![rucksack.0, rucksack.1];
+        let common_items = get_unique(&r[..]);
         for i in common_items {
             sum += i as i32;
         }
@@ -39,22 +40,53 @@ fn to_priority(i: u8) -> Result<u8, &'static str> {
     }
 }
 
-fn get_unique(r: Rucksack) -> Vec<u8> {
-    println!("{:?}", r);
-    let mut unique_map: HashSet<u8> = HashSet::with_capacity(26 * 2); // alphabet * 2
-    for item in r.0.as_bytes().iter() {
-        unique_map.insert(*item);
+fn get_badges(rs: &[Rucksack]) -> Vec<u8> {
+    let mut result = vec![];
+    for triplet in rs.chunks(3) {
+        let triplets = triplet
+            .iter()
+            .map(|r| format!("{}{}", r.0, r.1))
+            .collect::<Vec<String>>();
+
+        let striplets = triplets.iter().map(|s| s.as_str()).collect::<Vec<&str>>();
+
+        let badge = get_unique(&striplets[..]);
+        println!("badges: {:?}", &badge);
+
+        result.extend(badge);
+    }
+    result
+}
+
+fn get_unique(strings: &[&str]) -> Vec<u8> {
+    let mut sorted: Vec<u8> = vec![];
+    for r in strings {
+        let mut bytes = r.as_bytes().to_vec();
+        bytes.sort();
+        bytes.dedup();
+        sorted.extend(bytes);
     }
 
-    r.1.as_bytes()
-        .iter()
-        .copied()
-        .filter(|b| unique_map.contains(b))
-        .collect::<HashSet<u8>>()
-        .into_iter()
-        .collect::<Vec<u8>>()
-        .into_iter()
-        .map(|p| to_priority(p).unwrap())
+    println!("{:?}", &sorted);
+
+    let mut map = HashMap::<u8, usize>::new();
+
+    for r in sorted.iter() {
+        match map.get(r) {
+            None => {
+                map.insert(*r, 1);
+            }
+            Some(v) => {
+                map.insert(*r, v + 1);
+            }
+        }
+    }
+
+    println!("{:?}", &map);
+
+    map.iter()
+        .filter(|&(_, v)| strings.len() == *v)
+        .map(|(k, _)| *k)
         .collect()
 }
 
@@ -92,13 +124,39 @@ mod tests {
         assert!(input_error.is_none());
 
         let rucksacks = parse_input(input_data.as_ref().unwrap().as_str());
-        let mut sum = 0;
+        let mut sum: i32 = 0;
         for rucksack in rucksacks {
-            let common_items = get_unique(rucksack);
+            let r = vec![rucksack.0, rucksack.1];
+            let common_items = get_unique(&r[..]);
+            println!("{:?}", common_items);
             for i in common_items {
-                sum += i;
+                println!("{}", i);
+                match to_priority(i) {
+                    Ok(v) => sum += v as i32,
+                    Err(e) => panic!("{}", e),
+                }
             }
         }
+        println!("sum: {}", &sum);
         assert_eq!(sum, 157);
+    }
+
+    #[test]
+    fn test_part_two() {
+        let input_data = read_input("include/example");
+        let input_error = input_data.as_ref().err();
+        assert!(input_error.is_none());
+
+        let rucksacks = parse_input(input_data.as_ref().unwrap().as_str());
+        let badges = get_badges(&rucksacks);
+
+        let mut sum = 0;
+        for badge in badges {
+            match to_priority(badge) {
+                Ok(v) => sum += v as i32,
+                Err(e) => panic!("{}", e),
+            }
+        }
+        assert_eq!(sum, 70);
     }
 }
